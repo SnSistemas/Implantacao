45e82c8dac2f6d8756a57982e7aa4ba68ea27ca5
Sn Sistemas <75288283+SnSistemas@users.noreply.github.com>
2021-02-18 08:34:16 -0300

    Update BUSCAR_PESSOA_NULL.sql

--- Scripts/BUSCAR_PESSOA_NULL.sql
+++ Scripts/BUSCAR_PESSOA_NULL.sql
@@ -1,3 +1,7 @@
+/*
+* @author: Ruan Alves
+* Função: Verifica na tabela pessoa se alguns campos se encontram NULL, pois a falta dessas informações impossibilita a transmissão da NOTA FISCAL
+*/
 SELECT P.CODIGO, P.NOME, P.FANTASIA, P.ENDERECO, P.COMPLEMENTO, P.BAIRRO, P.CODCIDADE FROM PESSOA P
 	WHERE P.NOME IS NULL OR P.FANTASIA IS NULL OR P.ENDERECO IS NULL OR P.COMPLEMENTO IS NULL OR P.BAIRRO IS NULL OR P.CODCIDADE IS NULL
 	AND P.CNPJ IS NOT NULL
