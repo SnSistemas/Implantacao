0f64b2350788416b9b54677218ce71fc3f4426fa
Sn Sistemas <75288283+SnSistemas@users.noreply.github.com>
2021-02-20 19:19:53 -0300

    Add files via upload

---
+++ Scripts/UPDATE_CXBANCO_BB240.sql
@@ -0,0 +1,5 @@
+/*
+* @author: Ruan Alves
+* Função: Se o cliente utiliza Banco do Brasil CNAB 240, deverá rodar esse Script
+*/
+UPDATE CXBANCO SET VERSAOLAYOUT = '030' WHERE CODIGO = (CODIGO DO BANCO)
